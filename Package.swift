d215bfbfa2b71240182feaed940ff8c6fc65f14c
Vivek <iamviveklalan@gmail.com>
2024-12-10 11:35:32 +0530

    update sdk
    encryption

--- Package.swift
+++ Package.swift
@@ -4,14 +4,33 @@
 import PackageDescription
 
 let package = Package(
-  name: "SecuredCallsSDK",
-  products: [
-    .library(
-      name: "SecuredCallsSDK",
-      targets: ["SecuredCallsSDK"])
-  ],
-  targets: [
-    .binaryTarget(
-      name: "SecuredCallsSDK",
-      path: "./SDK/SecuredCallsSDK.xcframework.zip")
-  ])
+	name: "SecuredCallsSDK",
+	platforms: [
+		.iOS(.v16)
+	],
+	products: [
+		.library(
+			name: "SecuredCallsSDK",
+			targets: ["SecuredCallsSDKWrapper"]
+		)
+	],
+	dependencies: [
+		.package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
+		.package(url: "https://github.com/Swinject/SwinjectAutoregistration.git", from: "2.8.4")
+	],
+	targets: [
+		.binaryTarget(
+			name: "SecuredCallsSDKBinary",
+			path: "SDK/SecuredCallsSDK.xcframework"
+		),
+		.target(
+			name: "SecuredCallsSDKWrapper",
+			dependencies: [
+				"SecuredCallsSDKBinary",
+				.product(name: "Swinject", package: "Swinject"),
+				.product(name: "SwinjectAutoregistration", package: "SwinjectAutoregistration")
+			],
+			path: "./Sources/SecuredCallsSDKWrapper"
+		)
+	]
+)
---
+++ SDK/SecuredCallsSDK.xcframework/ios-arm64_x86_64-simulator/SecuredCallsSDK.framework/Headers/SecuredCallsSDK.h
@@ -0,0 +1,18 @@
+//
+//  SecuredCallsSDK.h
+//  SecuredCallsSDK
+//
+//  Created by Vivek Lalan on 29/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecuredCallsSDK.
+FOUNDATION_EXPORT double SecuredCallsSDKVersionNumber;
+
+//! Project version string for SecuredCallsSDK.
+FOUNDATION_EXPORT const unsigned char SecuredCallsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecuredCallsSDK/PublicHeader.h>
+
+
